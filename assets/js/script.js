601697e6e70f08d607c2f61f419ded5767d125c1
John Napoles <84890923+MinecraftJohn@users.noreply.github.com>
2023-01-28 16:25:20 +0800

    partially added the dropdown with toglge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var theme = document.getElementById("theme"),
-html = document.documentElement;
+    html = document.documentElement;
 
 theme.onclick = () => {
     if (theme.checked) {
@@ -7,4 +7,27 @@ theme.onclick = () => {
     } else {
         html.setAttribute("theme", "light");
     }
+}
+
+const selects = document.getElementsByClassName("dropdown");
+
+for (let i = 0; i < selects.length; i++) {
+    const select = selects[i];
+    const selected = select.getElementsByClassName("selected")[0];
+    const options = select.getElementsByClassName("options")[0];
+
+    select.addEventListener("click", () => {
+        options.style.display = options.style.display === "block" ? "none" : "block";
+    });
+
+    options.addEventListener("click", (event) => {
+        selected.innerHTML = event.target.innerHTML;
+        options.style.display = "none";
+    });
+
+    document.addEventListener("click", (event) => {
+        if (!select.contains(event.target)) {
+            options.style.display = "none";
+        }
+    });
 }
